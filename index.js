fb042932e9c0da38fc7555d4ab2d91127c34de1b
sivakrish1 <sivapalaparthi003@gmail.com>
2025-05-16 10:44:03 +0530

    Initial commit

--- index.js
+++ index.js
@@ -19,10 +19,35 @@ app.get("/", function (req, res) {
 });
 
 
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+//implementing the logic for the 1st project
+app.get('/api/:date?',(req,res)=>{
+  let dateString = req.params.date
+
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+  if(date.toString() === 'Invalid Date')
+    return res.json({error : 'Invalid Date'})
+
+  res.json({
+    unix : date.getTime(),
+    utc : date.toUTCString()
+  })
+})
+
 
 
 
